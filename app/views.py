a1751440cc1fdd71a599e93a6daa96887c7f0849
Ashish Sharma <ashish.paul.sharma@gmail.com>
2021-09-21 22:20:46 +0530

    Create views.py

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from app import app
+
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+    
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    app.run()
