29fa9ff9f80c8351c9cd9bc04f7b5b284319e5b1
Ashish Sharma <ashish.paul.sharma@gmail.com>
2021-09-21 21:00:52 +0530

    Update views.py

--- views.py
+++ views.py
@@ -5,3 +5,9 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.htm")
+    
+    
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    app.run()
